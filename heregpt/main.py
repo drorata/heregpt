1edda7a77ef6be0279a726248b62a3da35f40d29
Dror Atariah <drorata@gmail.com>
2023-07-11 22:29:36 +0200

    Added CLI option to get OpenAI key
    Added help strings to the CLI arguments

--- heregpt/main.py
+++ heregpt/main.py
@@ -1,6 +1,9 @@
+import os
+
 import typer
 from rich import print
 from rich.console import Console
+from typing_extensions import Annotated
 
 from heregpt import utils
 from heregpt.models import TaskBase
@@ -11,13 +14,23 @@ console = Console()
 
 
 @app.command()
-def main(tool: str, task: str):
-    if not utils.set_openai_api_key():
-        console.print(
-            "The environment variable OPENAI_API_KEY is not defined. More details here:"
-        )
-        console.print(utils.set_openai_api_key.__doc__)
-        raise typer.Exit(42)
+def main(
+    tool: Annotated[str, typer.Argument(help="Name of the tool you want to use")],
+    task: Annotated[str, typer.Argument(help="Describe the task you want to execute")],
+    openai_key: Annotated[
+        str, typer.Option(help="Manually provided API key for OpenAI")
+    ] = None,
+):
+    if openai_key is not None:
+        os.environ["OPENAI_API_KEY"] = openai_key
+    if openai_key is None:
+        if not utils.set_openai_api_key():
+            console.print(
+                "The environment variable OPENAI_API_KEY is not defined. More details"
+                " here:"
+            )
+            console.print(utils.set_openai_api_key.__doc__)
+            raise typer.Exit(42)
     task = TaskBase(tool=tool, task=task)
     task.build_prompt()
     console.print("About to send the following prompt🚀", style="#5f5fff")
